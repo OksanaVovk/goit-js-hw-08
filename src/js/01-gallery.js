564744b5328245ca023bb4daedb535098111fa79
OksanaVovk <oksvovk85@gmail.com>
2022-04-24 17:41:53 +0300

    зроблено перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryCards = createGalleryCard(galleryItems);
+galleryEl.innerHTML = galleryCards;
+
+function createGalleryCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}"alt="${description}" />
+  </a>
+  </div>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a');
+lightbox.on('show.simplelightbox', function () {
+  lightbox.options.captionsData = 'alt';
+  lightbox.options.captionDelay = '250ms';
+});
