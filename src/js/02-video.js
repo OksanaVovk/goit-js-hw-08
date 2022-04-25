f7905842f2497adc359eb6473b6ca5ee88846460
OksanaVovk <oksvovk85@gmail.com>
2022-04-26 02:17:23 +0300

    Завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(playerOn, 1000));
+function playerOn(event) {
+  console.log(event);
+  localStorage.setItem(VIDEO_CURRENT_TIME, JSON.stringify(event));
+}
+playerRestart();
+function playerRestart() {
+  const savedTime = localStorage.getItem(VIDEO_CURRENT_TIME);
+  const parsedTime = JSON.parse(savedTime);
+  if (localStorage.getItem(VIDEO_CURRENT_TIME)) {
+    console.log(parsedTime.seconds);
+  }
+  player.setCurrentTime(parsedTime.seconds);
+}
