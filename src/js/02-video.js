649857b4e2668404ca85d01c023b1a19877309f9
OksanaVovk <oksvovk85@gmail.com>
2022-05-08 11:12:30 +0300

    додані проміси у функцію

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -16,7 +16,19 @@ function playerRestart() {
   const savedTime = localStorage.getItem(VIDEO_CURRENT_TIME);
   const parsedTime = JSON.parse(savedTime);
   if (localStorage.getItem(VIDEO_CURRENT_TIME)) {
-    console.log(parsedTime.seconds);
-    player.setCurrentTime(parsedTime.seconds);
+    player
+      .setCurrentTime(parsedTime.seconds)
+      .then(value => {
+        console.log(value);
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            console.log(error);
+            break;
+          default:
+            break;
+        }
+      });
   }
 }
